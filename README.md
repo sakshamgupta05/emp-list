d02f5bbd1e161f0b5216499ed7b9b49c150c624d
Saksham Gupta <sakshamgupta05@gmail.com>
2018-11-30 18:31:57 +0530

    proj setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# emp-list
+A java swing app to manage employee database
+
+**Latest release** - [EmpList.jar](https://github.com/sakshamgupta05/emp-list/releases/latest)
---
+++ src/emplist/EmpList.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package emplist;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Saksham
+ */
+public class EmpList {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.sql.SQLException
+     */
+    public static void main(String[] args)  {
+        NewJFrame cal=new NewJFrame();
+        cal.setVisible(true);
+    }
+    
+}
